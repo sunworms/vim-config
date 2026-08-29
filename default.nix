1fd49b56efe9e92e5ab65c232421dc5847b4c7c1
Sunny <sunnybhowmick0310@gmail.com>
2026-08-29 20:43:21 +0530

    made config able to follow passed nixpkgs

--- default.nix
+++ default.nix
@@ -1,16 +1,23 @@
-let
+{pkgs ? null}: let
   inputs = import ./_sources/generated.nix {
     fetchurl = null;
     fetchgit = null;
     fetchFromGitHub = null;
     dockerTools = null;
   };
-  pkgs = import inputs.nixpkgs.src {
-    config.allowUnfree = true;
-  };
+  finalPkgs =
+    if pkgs != null
+    then pkgs
+    else
+      import inputs.nixpkgs.src {
+        config.allowUnfree = true;
+      };
   vim-dir = ./config;
-  vimRcFile = pkgs.runCommand "vimrc.vim" {} ''
+  vimRcFile = finalPkgs.runCommand "vimrc.vim" {} ''
     cat ${vim-dir}/* > $out
   '';
 in
-(import ./package.nix {inherit pkgs vimRcFile;})
+  (import ./package.nix {
+    pkgs = finalPkgs;
+    inherit vimRcFile;
+  })
